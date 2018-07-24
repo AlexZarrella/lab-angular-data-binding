cbd003c3a1069945163550192bb653a17eab9929
alexzarrella <alexzarrella@Alexs-MacBook-Pro-3.local>
2018-07-24 16:20:26 -0400

    finished iteration 1

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,40 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+foods: Array<any>= [];
+newFoods: Object = {};
+
 
   constructor() { }
 
+
   ngOnInit() {
+    this.foods = foods;
   }
+
+//   searchMeal(input){
+//     // console.log("You searched for: ", input.value)
+//     this.foods.forEach(function(eachName){
+//       const newArray = eachName.name.filter(function(){
+//         console.log(eachName.name);
+//         if (eachName.name === input.value) {
+//           newArray.push(eachName);
+  
+//         }
+//       })
+
+      
+
+//     });
+//   }
+
+searchMeal(input){
+  const newArray = this.foods.filter(function(eachMeal){
+    if (eachMeal.name === input.value) {
+      newArray.push(eachMeal.name);
+      console.log(newArray);
+    }
+  })
+}
 
 }
